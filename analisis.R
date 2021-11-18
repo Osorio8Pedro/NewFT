3453747738957068e2a2636a3604e9a9ef5bc792
Pedro <pedro.osorio@usach.cl>
2021-11-18 02:22:13 -0300

    análisis descriptivo

--- analisis.R
+++ analisis.R
@@ -20,3 +20,68 @@ t2=aggregate(cant_casosconfirmadoschowellw5~id_semanaepidemiologica,ftis3,sum)
 t2
 plot(t1[,1],t1[,2],type="l")
 lines(t2[,1],t2[,2],col="red")
+
+
+############################
+########################
+##Análisis Descriptivo
+
+
+###Cuerpo
+ install.packages("remotes")
+remotes::install_github("jespermaag/gganatogram")
+library(gganatogram)
+ install.packages("dplyr")
+library(dplyr)
+
+hgMale_key %>%
+   filter(organ %in% c( "lung")) %>%
+   gganatogram(organism = "human", sex = "male",
+               fill = "colour") +
+   theme_void() + 
+   coord_fixed()
+
+hgMale_key %>%
+   filter(type %in% "nervous_system") %>%
+   gganatogram(organism = "human", sex = "male",
+               fill = "colour", outline = FALSE) +
+   theme_void() + 
+   coord_fixed()
+
+##calendario
+
+install.packages("calendR")
+library(calendR)
+
+# Datos
+datos <- rnorm(30, 15, 10)
+
+# Crea un vector donde todos los valores son ligeramente
+# inferiores que el menor valor de tus datos
+dias <- rep(min(datos) - 0.05, 365)
+
+# Rellena los días que quieras con tus datos
+dias[30:59] <- datos
+
+calendR(year = 2021,
+        special.days = dias,
+        low.col = "white",
+        special.col = "#FF0000",
+        gradient = TRUE,
+        legend.pos = "bottom")
+###########
+night_owlish <- "https://raw.githubusercontent.com/batpigandme/night-owlish/master/rstheme/night-owlish.rstheme"
+rstudioapi::addTheme(night_owlish, apply = TRUE)
+##########
+set.seed(2)
+ftregion<-read.csv("newftregion.txt",sep=";",header=T)
+names(ftregion)
+data_set <- data.frame(Region = ftregion$txt_nombreregion,
+   poblacion = ftregion$cant_poblacion,
+                       type = sample(1:4, size = 25, replace = TRUE),
+                       store = sample(paste("Store", 1:4),
+                                      size = 25, replace = TRUE))
+
+head(data_set)
+
+#############
